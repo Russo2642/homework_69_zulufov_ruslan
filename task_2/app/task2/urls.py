2f0f13c75abc5c457ae086d2175694ce52a71a6d
Ruslan Zulufov <gothiq2302@gmail.com>
2023-04-14 16:27:39 +0600

    Переделал второе задание

--- task_2/app/task2/urls.py
+++ task_2/app/task2/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from task2.views import index_view
+from task2.views import index_view, add_view, sub_view, multi_view, divide_view
 
 urlpatterns = [
     path('', index_view, name='index'),
+    path('add/', add_view, name='add'),
+    path('subtract/', sub_view, name='subtract'),
+    path('multiply/', multi_view, name='multiply'),
+    path('divide/', divide_view, name='divide'),
 ]
--- task_2/app/task2/views.py
+++ task_2/app/task2/views.py
@@ -1,5 +1,79 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+import json
 
 
 def index_view(request):
     return render(request, 'index.html')
+
+
+def add_view(request, *args, **kwargs):
+    result = 0
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            number_1 = float(data.get('number_1'))
+            number_2 = float(data.get('number_2'))
+            result = number_1 + number_2
+            answer = {
+                'result': result
+            }
+            return JsonResponse(answer, safe=False)
+        except Exception as e:
+            response = JsonResponse({'error': str(e)})
+            response.status_code = 400
+            return response
+        
+
+def sub_view(request, *args, **kwargs):
+    result = 0
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            number_1 = float(data.get('number_1'))
+            number_2 = float(data.get('number_2'))
+            result = number_1 - number_2
+            answer = {
+                'result': result
+            }
+            return JsonResponse(answer, safe=False)
+        except Exception as e:
+            response = JsonResponse({'error': str(e)})
+            response.status_code = 400
+            return response
+        
+
+def multi_view(request, *args, **kwargs):
+    result = 0
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            number_1 = float(data.get('number_1'))
+            number_2 = float(data.get('number_2'))
+            result = number_1 * number_2
+            answer = {
+                'result': result
+            }
+            return JsonResponse(answer, safe=False)
+        except Exception as e:
+            response = JsonResponse({'error': str(e)})
+            response.status_code = 400
+            return response
+        
+
+def divide_view(request, *args, **kwargs):
+    result = 0
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        try:
+            number_1 = float(data.get('number_1'))
+            number_2 = float(data.get('number_2'))
+            result = number_1 / number_2
+            answer = {
+                'result': result
+            }
+            return JsonResponse(answer, safe=False)
+        except Exception as e:
+            response = JsonResponse({'error': str(e)})
+            response.status_code = 400
+            return response
